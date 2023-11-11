36eba0e22e1a5910467ee83b5f28d93ea0b6870c
ismatzoda5 <husein.ismatzoda@icloud.com>
2023-11-11 15:36:24 +0500

    add

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//     screens: {
+//       "phone": { 'max': '400px'},
+//     }
+//   },
+//   plugins: [],
+// }
+
